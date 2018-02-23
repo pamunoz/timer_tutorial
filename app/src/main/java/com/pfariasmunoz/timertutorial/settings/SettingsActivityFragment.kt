c6c540a093b387c07c1e542f6d18c50fb79bd3ff
pamunoz <pfariasmunoz@gmail.com>
2018-02-23 16:00:32 -0300

    Add Preference Fragment to layout and as a class

---
+++ app/src/main/java/com/pfariasmunoz/timertutorial/settings/SettingsActivityFragment.kt
@@ -0,0 +1,14 @@
+package com.pfariasmunoz.timertutorial.settings
+
+import android.os.Bundle
+import android.support.v7.preference.PreferenceFragmentCompat
+
+/**
+ * Created by GAMER on 23-02-2018.
+ */
+class SettingsActivityFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+    }
+
+}
